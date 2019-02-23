6ba06993a284249652a3ab32a7e12b31b8f37027
Ivan Budanov <32747977+iabudanov@users.noreply.github.com>
2019-02-23 13:25:12 +0000

    Add files via upload

---
+++ sum_of_intervals.py
@@ -0,0 +1,11 @@
+def sum_of_intervals(intervals):
+    listy = []
+    for i in intervals:
+        for num in range(i[0], i[1]):
+            listy.append(num)
+    listy = set(listy)
+    num = len(listy)
+    return num
+
+
+#https://www.codewars.com/kata/52b7ed099cdc285c300001cd/solutions/python
