835e7d9246fce0b970ac0492fee718ff08a1e783
Ivan Budanov <32747977+iabudanov@users.noreply.github.com>
2019-02-23 13:20:58 +0000

    Update parentheses_checker.py

--- parentheses_checker.py
+++ parentheses_checker.py
@@ -1,1 +1,28 @@
-# Codewars_Problems
+def group_check(xs):
+    index = 0
+    p_dict = {
+        '(' : ')',
+        '[':']',
+        '{':'}'
+    }
+    xs = list(xs)
+    while True:
+        print(index)
+        print(xs)
+        current_left = xs[index]
+        try:
+            current_right = xs[index + 1]
+        except IndexError:
+            return False
+        if p_dict[current_left] == current_right:
+            del xs[index]
+            del xs[index]
+            if index == 0:
+                return True
+            index -= 1
+
+        elif p_dict[xs[index]] not in p_dict:
+            return False
+
+        else:
+            index += 1
